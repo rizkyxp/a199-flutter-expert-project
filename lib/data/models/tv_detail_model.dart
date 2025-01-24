f6c4d3793dad658bc7ecbd35cd050bd3f7aa6b2b
Rizky Pratama <rizkyp191@gmail.com>
2025-01-24 10:01:22 +0700

    handle null data api error

--- lib/data/models/tv_detail_model.dart
+++ lib/data/models/tv_detail_model.dart
@@ -4,15 +4,15 @@ import 'package:equatable/equatable.dart';
 
 class TvDetailResponse extends Equatable {
   final bool adult;
-  final String backdropPath;
+  final String? backdropPath;
   final List<int> episodeRunTime;
   final String firstAirDate;
   final List<GenreModel> genres;
-  final String homepage;
+  final String? homepage;
   final int id;
   final bool inProduction;
   final List<String> languages;
-  final String lastAirDate;
+  final String? lastAirDate;
   final String name;
   final dynamic nextEpisodeToAir;
   final int numberOfEpisodes;
@@ -20,12 +20,12 @@ class TvDetailResponse extends Equatable {
   final List<String> originCountry;
   final String originalLanguage;
   final String originalName;
-  final String overview;
+  final String? overview;
   final double popularity;
-  final String posterPath;
-  final String status;
-  final String tagline;
-  final String type;
+  final String? posterPath;
+  final String? status;
+  final String? tagline;
+  final String? type;
   final double voteAverage;
   final int voteCount;
 
